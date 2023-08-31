ca42482d914baa1cefff994af84201693b78da0d
b3v21 <b3nn2121@gmail.com>
2023-08-31 17:13:36 +1000

    feat: testdata2 implemented more but not fully functional. will continue to work on it.

--- data/testdata2.py
+++ data/testdata2.py
@@ -20,11 +20,11 @@ std = [random.randint(0,20) for _ in F]
 sta = [std[i]+round(random.uniform(0,4),2) for i in F]
 
 # Arrival and Depature slots
-AA = [[random.randint(0,21), _] for _ in range(90)]
+AA = [[random.randint(0,21), _] for _ in range(300)]
 for aa in AA:
     aa[1] = aa[0]+round(random.uniform(0,3),2)
     
-DA = [[random.randint(0,21), _] for _ in range(90)]
+DA = [[random.randint(0,21), _] for _ in range(300)]
 for da in DA:
     da[1] = da[0]+round(random.uniform(0,3),2)
 
@@ -42,39 +42,52 @@ F_t = [random.sample(F, random.randint(0,5)) for _ in T]
 # set of tails compatible with flight F
 T_f = [[t for t in T if f in F_t[t]] for f in F]
 
-# Set of flights compatible with a connection from flight f
-CF_f = [[1], [], [3], []]
-
 # Set of flights f which arrive to airport k
 flights = [f for f in F]
-FA_k = []
+FA_k = {}
+AK_f = {} # Airport that flight f arrives at (this isnt actually data in the paper)
+
 while flights:
     for k in K:
         sample = random.sample(flights,random.randint(0,min(8,len(flights))))
-        FA_k += [sample]
+        FA_k[k] = sample
         for s in sample:
-            flights.remove(s)
+            AK_f[s] = k
+            flights.remove(s)   
 
 # Set of flights f which depart from airport k
-FD_k = [[0, 2], [1, 3], []]
+FD_k = {}
+for k in K:
+    FD_k[k] = set()
+for k in K:
+    for f in F:
+        for p in P:
+            if f not in p:
+                continue
+            for i,_ in enumerate(p):
+                if p[i] == f:
+                    FD_k[AK_f[p[i-1]]].add(f)
+            
+# Set of flights compatible with a connection from flight f
+CF_f = [[fd for fd in F if fd == AK_f[f] and fd != f] for f in F]
 
 # Subset of itineraries compatible with a reasignment from an original itinerary p.
-CO_p = [[0], [1]]
+CO_p = [[i] for i,p in enumerate(P)] # Currently can only reschedule to itself
 
 # Data
 
 # Cost of operating flight f with tail t
-oc = [[1 for _ in range(num_flights)] for _ in range(num_tails)]
+oc = [[random.randrange(0,50,5) for _ in F] for _ in T]
 
 # Delay cost per minute of arrival delay of flight f
-dc = [100 for _ in range(num_flights)]
+dc = [100 for _ in F]
 
 # Number of passengers in fare class v that are originally scheduled to
 # take itinerary p
-n = [[100, 100]]
+n = [[random.randint(50,300) for _ in P] for _ in Y]
 
 # Seating capacity of tail t in T
-q = [100, 100]
+q = [100 for _ in T]
 
 # Reaccommodation Cost for a passenger reassigned from p to pd.
 rc = [[(lambda p, pd: 0 if p == pd else 0.5)(p, pd) for p in P] for pd in P]
@@ -82,12 +95,13 @@ rc = [[(lambda p, pd: 0 if p == pd else 0.5)(p, pd) for p in P] for pd in P]
 # Phantom rate for passenger in fare class v reassigned from p to pd with delay level
 # zeta
 theta = [
-    [[[0 for _ in Y] for _ in range(len(P))] for _ in range(len(P))] for _ in Z
+    [[[0 for _ in Y] for _ in P] for _ in P] for _ in Z
 ]
 
-# Starting location of planes (binary)
-tb = [[1, 0, 0], [1, 0, 0]]
+# Starting location of planes (binary) ((for t) for k)
+tb = [random.sample([1] + [0 for _ in range(len(T)-1)],len(T)) for _ in K]
+print(tb)
 
 # Capacity of arrival and departure slots
-scA = [1, 1, 1, 1]
-scD = [1, 1, 1, 1]
+scA = [1 for _ in range(300)]
+scD = [1 for _ in range(300)]

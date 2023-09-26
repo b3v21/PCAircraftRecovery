280064794657200c35697d8c6b1951e38db9de1b
b3v21 <b3nn2121@gmail.com>
2023-09-26 15:30:00 +1000

    feat: data extraction begun for pseudo_aus example

--- data/test_pseudo_aus.py
+++ data/test_pseudo_aus.py
@@ -27,6 +27,7 @@
 
 import copy
 import random
+import numpy as np
 
 random.seed(69)
 
@@ -34,7 +35,8 @@ TIME_HORIZON = 72
 
 
 class Node:
-    def __init__(self, name, lat, long, time):
+    def __init__(self, node_id, name, lat, long, time):
+        self.id = node_id
         self.name = name
         self.lat = lat
         self.long = long
@@ -52,27 +54,37 @@ class Node:
     
     def __hash__(self) -> int:
         return hash(str(self))
+    
+    def get_id(self):
+        return self.id
 
-    def new_time_copy(self, time):
+    def new_time_copy(self, time, node_id):
         new_node = copy.deepcopy(self)
         new_node.time = time
+        new_node.id = node_id
         return new_node
+    
+    def get_time(self):
+        return self.time
 
 
 class AdjanecyList:
     def __init__(self):
         self.adj_list = {}
 
-    def add_node(self, node, neighbours):
+    def add_node(self, node, neighbours, flight_id):
         self.adj_list[node] = []
         for neighbour in neighbours:
-            self.adj_list[node].append(neighbour)
+            self.adj_list[node].append((neighbour, flight_id))
 
     def get_node(self, name):
         for node in self.adj_list:
             if node.name == name:
                 return node
         return None
+    
+    def add_to_node(self, name, neighbour, flight_id):
+        self.adj_list[name].append((neighbour, flight_id))
 
     def get_neighbours(self, node):
         return self.adj_list[node]
@@ -96,24 +108,26 @@ class AdjanecyList:
 def create_graph():
     # Create default nodes
     default_nodes = [
-        Node("SYD", -33.949995, 151.181705, None),
-        Node("BNE", -27.386944, 153.1175, None),
-        Node("MEL", -37.673333, 144.843333, None),
-        Node("PER", -31.940278, 115.966944, None),
-        Node("ADL", -34.945, 138.530556, None),
-        Node("OOL", -28.164444, 153.505278, None),
-        Node("CNS", -16.885833, 145.755278, None),
-        Node("HBA", -42.836111, 147.510278, None),
-        Node("CBR", -35.306944, 149.195, None),
-        Node("TSV", -19.2525, 146.765278, None),
+        Node(0, "SYD", -33.949995, 151.181705, None),
+        Node(0,"BNE", -27.386944, 153.1175, None),
+        Node(0,"MEL", -37.673333, 144.843333, None),
+        Node(0,"PER", -31.940278, 115.966944, None),
+        Node(0,"ADL", -34.945, 138.530556, None),
+        Node(0,"OOL", -28.164444, 153.505278, None),
+        Node(0,"CNS", -16.885833, 145.755278, None),
+        Node(0,"HBA", -42.836111, 147.510278, None),
+        Node(0,"CBR", -35.306944, 149.195, None),
+        Node(0,"TSV", -19.2525, 146.765278, None),
     ]
 
+    current_node_id = 0
+    current_flight_id = 0
     adj_list = AdjanecyList()
-    flights_remaining = int(random.normalvariate(1000, 100))
+    flights_remaining = int(random.normalvariate(20, 8))
     
     for node in default_nodes:
         if node.name == "SYD":
-            flights_used = random.randrange(int(0.5*flights_remaining), int(0.81*flights_remaining))
+            flights_used = random.randrange(int(0.4*flights_remaining), int(0.61*flights_remaining))
         else:
             flights_used = random.randrange(0, int(0.3 * flights_remaining))
         
@@ -121,22 +135,60 @@ def create_graph():
             neighbours = []
             
             # Randomise destination airport
-            dest_node = random.choice(default_nodes)
+            dest_node = random.choices(default_nodes, weights=[0.25, 0.15, 0.15, 0.1, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05])[0]
             while dest_node == node:
-                dest_node = random.choice(default_nodes)
+                dest_node = random.choices(default_nodes, weights=[0.25, 0.15, 0.15, 0.1, 0.05, 0.05, 0.05, 0.05, 0.05, 0.05])[0]
 
             # Randomise time flight is scheduled to depart
             departure_time = round(random.random() * TIME_HORIZON, 1)
-            departure_node = node.new_time_copy(departure_time)
+            departure_node = node.new_time_copy(departure_time, current_node_id)
             
-            neighbours.append(dest_node.new_time_copy(departure_time + 2))
-            
-            adj_list.add_node(departure_node, neighbours)
+            if departure_node in adj_list.get_all_nodes():
+                adj_list.add_to_node(departure_node, dest_node.new_time_copy(departure_time + 2, current_node_id+1), current_flight_id)
+            else:
+                neighbours.append(dest_node.new_time_copy(departure_time + 2, current_node_id+1))
+                adj_list.add_node(departure_node, neighbours, current_flight_id)
 
+            current_node_id += 1
+            current_flight_id += 1
         flights_remaining -= flights_used
 
     return adj_list
 
+def extract_data(graph : AdjanecyList) -> None:
+    num_flights = sum(graph.count_node_locations().values())
+    num_tails = 20 # This is somewhat arbitrary (up to us to decide size of fleet)
+    num_airports = 10 
+    num_fare_classes = 2 # This is somewhat arbitrary
+    num_delay_levels = 2 # This is somewhat arbitrary
+
+    # Sets
+    T = range(num_tails)
+    F = range(num_flights)
+    K = range(num_airports)
+    Y = range(num_fare_classes)
+    Z = range(num_delay_levels)
+    
+    # Set of itineraries, this needs to be reworked for multi-leg itins
+    P = []
+    for n in graph.get_all_nodes():
+        for neigh, flight_id in graph.get_neighbours(n):
+            P.append([flight_id])  
+    
+    # Construct arrival and departure times 
+    # NOTE: THESE ARE LISTS IN PREV DATA FILES, NOW DICTS
+    std = {}
+    sta = {}
+    for n in graph.get_all_nodes():
+        for neigh, flight_id in graph.get_neighbours(n):
+            std[flight_id] = n.time
+            sta[flight_id] = neigh.time
+            
+    # Construct arrival and departure slots
+    DA = [(t, t + 0.25) for t in np.arange(0, TIME_HORIZON, 0.25)]
+    AA = [(t, t + 0.25) for t in np.arange(0, TIME_HORIZON, 0.25)]
+            
+    
 graph = create_graph()
 print(graph.count_node_locations())
-
+extract_data(graph)

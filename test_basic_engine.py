8c85069129a2e1e219bb5d7fc7a671e0677bd463
Harrison Bohl <bohlharrison357@gmail.com>
2023-10-13 22:45:50 +1000

    test: sussy basic maintenance test

--- test_basic_engine.py
+++ test_basic_engine.py
@@ -25,7 +25,7 @@ def test_basic_solve():
     num_airports = 3
     num_fare_classes = 2
     num_delay_levels = 2
-    num_time_instances = 2
+    num_time_instances = 8
 
     T = range(num_tails)
     F = range(num_flights)
@@ -46,7 +46,7 @@ def test_basic_solve():
     }
     K_m = set([k for k in K])
     T_m = set([])
-    PI_m = set([0,1])
+    PI_m = set([0, 1])
 
     abh = {t: 20 for t in T}
     sbh = {f: 20 for f in F}
@@ -905,3 +905,261 @@ def test_passenger_itinerary_disruption():
                         assert int(h[P.index(p), P.index(pd), v].x) == 25
                     else:
                         assert int(h[P.index(p), P.index(pd), v].x) == 0
+
+
+def test_basic_maintenance():
+    """
+    Ensures that a flight is rescheduled to a cheaper tail if it is available.
+    """
+
+    basic_maintenance = Model("test_basic_maintenance")
+
+    num_flights = 2
+    num_tails = 1
+    num_airports = 3
+    num_fare_classes = 2
+    num_delay_levels = 2
+    num_time_instances = 8
+
+    T = range(num_tails)
+    F = range(num_flights)
+    P = [[i, i + 1] for i in range(0, num_flights, 2)]
+    K = range(num_airports)
+    Y = range(num_fare_classes)
+    Z = range(num_delay_levels)
+
+    std = {0: 0.5, 1: 30.5}
+    sta = {0: 5.5, 1: 35.5}
+
+    # MAINTENANCE DATA / SETS
+    PI = range(num_time_instances)
+    MO = set([(f, fd) for f in F for fd in F if f != fd])
+    F_pi = {
+        pi: [f for f in F if sta[f] <= (1 + pi) * (TIME_HORIZON / num_time_instances)]
+        for pi in PI
+    }
+    K_m = set([k for k in K])
+    T_m = set(T)
+    PI_m = set([0, 1])
+
+    abh = {t: 20 for t in T}
+    sbh = {f: 20 for f in F}
+    mbh = {t: 40 for t in T}
+    mt = {t: 1 for t in T}
+    aw = {k: 1 for k in K}
+
+    DA = [(0, 1), (30, 31)]
+    AA = [(5, 6), (35, 36)]
+
+    AAF = {
+        f: [i for i, slot in enumerate(AA) if sta[f] <= slot[1] and sta[f] >= slot[0]]
+        for f in F
+    }
+    DAF = {
+        f: [i for i, slot in enumerate(DA) if std[f] <= slot[1] and std[f] >= slot[0]]
+        for f in F
+    }
+
+    FAA = {asl: [f for f in F if sta[f] <= asl[1] and sta[f] >= asl[0]] for asl in AA}
+    FDA = {dsl: [f for f in F if std[f] <= dsl[1] and std[f] >= dsl[0]] for dsl in DA}
+
+    F_t = {t: list(F) for t in T}
+    T_f = {f: [t for t in T if f in F_t[t]] for f in F}
+
+    flights = [f for f in F]
+    FA_k = {}
+    for k in K:
+        FA_k[k] = []
+
+    AK_f = {}
+
+    for f in F:
+        if f % 2 == 0:
+            FA_k[1].append(f)
+            AK_f[f] = 1
+        else:
+            FA_k[2].append(f)
+            AK_f[f] = 2
+
+    FD_k = {}
+    for k in K:
+        FD_k[k] = []
+
+    for f in F:
+        if f % 2 == 0:
+            FD_k[0].append(f)
+        else:
+            FD_k[1].append(f)
+
+    DK_f = {}
+    for f in F:
+        DK_f[f] = -1
+
+    for f in F:
+        for k in K:
+            if f in FD_k[k]:
+                DK_f[f] = k
+
+    CF_f = {
+        f: [fd for fd in F if AK_f[f] == DK_f[fd] and sta[f] <= std[fd] and fd != f]
+        for f in F
+    }
+
+    CO_p = {
+        P.index(p): [
+            P.index(pd)
+            for pd in P
+            if pd != []
+            and DK_f[pd[0]] == DK_f[p[0]]
+            and AK_f[pd[-1]] == AK_f[p[-1]]
+            and std[pd[0]] >= std[p[0]]
+            and sta[pd[-1]] >= sta[p[-1]]
+        ]
+        for p in P
+        if p != []
+    }
+    oc = {(t, f): 100 for f in F for t in T}
+    dc = {f: 100 for f in F}
+    n = {(v, P.index(p)): 50 for p in P for v in Y}
+    q = {t: 100 for t in T}
+    rc = {
+        (P.index(p), P.index(pd)): (lambda p, pd: 0 if p == pd else 0.5)(p, pd)
+        for p in P
+        for pd in P
+    }
+    theta = {
+        (v, P.index(p), P.index(pd), z): 0 for v in Y for p in P for pd in P for z in Z
+    }
+
+    tb = {(t, k): 1 if k == 0 else 0 for t in T for k in K}
+    scA = {asl: 1 for asl in AA}
+    scD = {dsl: 1 for dsl in DA}
+    sb = {f: 0 for f in F}
+    mtt = {(t, f, fd): 0 for t in T for f in F for fd in F}
+    mct = {(P.index(p), f, fd): 0 for p in P for f in F for fd in F}
+    ct = {(f, fd): max(0, std[fd] - sta[f]) for fd in F for f in F}
+    CF_p = {P.index(p): [(p[i], p[i + 1]) for i, _ in enumerate(p[:-1])] for p in P}
+    lf = {(P.index(p), f): 1 if p[-1] == f else 0 for p in P for f in F}
+    small_theta = {z: 1000 for z in Z}
+    fc = {f: 100 for f in F}
+    pc = {(z, P.index(p), P.index(pd)): 0 for z in Z for p in P for pd in P}
+    kappa = 100
+    x_hat = {(f, t): 1 if t == f or t + 1 == f else 0 for f in F for t in T}
+
+    variables = generate_variables(basic_maintenance, T, F, Y, Z, P, AA, DA, CO_p, K)
+    set_objective(
+        basic_maintenance,
+        variables,
+        T,
+        F,
+        Y,
+        Z,
+        P,
+        F_t,
+        CO_p,
+        oc,
+        dc,
+        rc,
+        theta,
+        fc,
+        pc,
+        kappa,
+        x_hat,
+    )
+    flight_scheduling_constraints(basic_maintenance, variables, F, T_f)
+    sequencing_and_fleet_size_constraints(
+        basic_maintenance, variables, T, F, K, F_t, T_f, FD_k, CF_f, tb
+    )
+    passenger_flow_constraints(
+        basic_maintenance, variables, F, Y, Z, P, T_f, CO_p, theta, n, q
+    )
+    airport_slot_constraints(
+        basic_maintenance,
+        variables,
+        F,
+        Z,
+        sta,
+        std,
+        AA,
+        DA,
+        AAF,
+        DAF,
+        FAA,
+        FDA,
+        scA,
+        scD,
+    )
+    flight_delay_constraints(basic_maintenance, variables, T, F, T_f, CF_f, sb, mtt, ct)
+    itinerary_feasibility_constraints(
+        basic_maintenance, variables, F, P, sta, std, CF_p, mct
+    )
+    itinerary_delay_constraints(
+        basic_maintenance, variables, F, Z, P, sta, CO_p, lf, small_theta
+    )
+    maintenance_schedule_constraints(
+        basic_maintenance, variables, T_m, sta, T_f, F, F_t, mt, MO, std
+    )
+    workshop_schedule_constraints(
+        basic_maintenance, variables, F_t, T_m, K_m, F, T_f, K, aw, FA_k
+    )
+    maintenance_check_constraints(
+        basic_maintenance, variables, T_m, PI_m, F_pi, sbh, mbh, F_t, MO, abh, F, T_f
+    )
+    beta_linearizing_constraints(basic_maintenance, variables, Y, Z, P, CO_p)
+
+    print("optimizing...")
+    basic_maintenance.optimize()
+
+    print("generating output...")
+    generate_output(
+        basic_maintenance,
+        variables,
+        T,
+        F,
+        Y,
+        Z,
+        P,
+        sta,
+        std,
+        AA,
+        DA,
+        DK_f,
+        AK_f,
+        CF_f,
+        n,
+        fc,
+        T_m,
+    )
+
+    (
+        _,
+        z,
+        _,
+        _,
+        _,
+        _,
+        _,
+        lambd,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+        _,
+    ) = variables
+
+    # No flights cancelled and no itineraries disrupted
+    for f in F:
+        assert z[f].x < 0.9
+    for p in P:
+        assert lambd[P.index(p)].x < 0.9
